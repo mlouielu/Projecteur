256f5fa3875fca2979c7302422639592b380e390
Jahn <jahnf@users.noreply.github.com>
2019-11-09 11:59:13 +0100

    Add libusb 1.0 devel to all images

--- cmake/modules/PkgDependenciesProjecteur.cmake
+++ cmake/modules/PkgDependenciesProjecteur.cmake
@@ -1,19 +1,23 @@
 list(APPEND _PkgDeps_Projecteur_opensuse
   "libqt5-qtgraphicaleffects >= 5.7"
   "libQt5Widgets5 >= 5.7"
+  "libQt5X11Extras5 >= 5.7"
   "shadow"
   "udev"
+  "libusb-1_0-0"
 )
 
 list(APPEND _PkgDeps_Projecteur_fedora
   "qt5 >= 5.7"
   "passwd"
+  "libusbx"
   "udev"
 )
 
 list(APPEND _PkgDeps_Projecteur_debian
   "qml-module-qtgraphicaleffects (>= 5.7)"
   "libqt5widgets5 (>= 5.7)"
+  "libusb-1.0-0"
   "passwd"
   "udev"
 )
@@ -22,6 +26,7 @@ list(APPEND _PkgDeps_Projecteur_archlinux
   "qt5-base>=5.7"
   "qt5-declarative>=5.7"
   "qt5-graphicaleffects>=5.7"
+  "libusb"
   "udev"
 )
 
